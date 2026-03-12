80f09a947f72c52cab6cad724f31b9bb8fecdbd0
Shubham Thakur <shubhamthakur@Shubhams-MacBook-Pro.local>
2026-03-12 22:41:41 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css'
-import ProductOne from "./assets/components/ProductOne/ProductOne.jsx";
-import ProductTwo from "./assets/components/ProductTwo/ProductTwo.jsx";
-import ProductThree from "./assets/components/ProductThree/ProductThree.jsx";
+import ProductOne from "./components/ProductOne/ProductOne.jsx";
+import ProductTwo from "./components/ProductTwo/ProductTwo.jsx";
+import ProductThree from "./components/ProductThree/ProductThree.jsx";
 
 const App = () =>{
   return(
